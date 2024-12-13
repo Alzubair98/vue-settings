cf0df05a9184fd5ef825dfd5c53f7498d1c7eca6
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-13 19:48:27 +0300

    add notification to all comps

--- src/components/NotificationsSettings.vue
+++ src/components/NotificationsSettings.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2 class="text-2xl mb-4">Notifications</h2>
-    <form class="space-y-4 mx-auto">
+    <form class="space-y-4 mx-auto" @submit.prevent="save">
       <div class="flex items-center gap-1">
         <input type="checkbox" v-model="notification.email" />
         <label> Email Notification </label>
@@ -17,6 +17,12 @@
 
 <script lang="ts" setup>
 import { useSettings } from '../composables/useSettings'
+import { useNotifications } from '../composables/useNotifications'
 
 const { notification } = useSettings()
+const { addNotification } = useNotifications()
+
+const save = () => {
+  addNotification('Notifications Saved')
+}
 </script>
